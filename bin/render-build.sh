3184f2a582247e3051464ffa355d7a9f1a80471c
Oliver KEB <oliver.keb@icloud.com>
2023-03-30 14:07:22 +0200

    🐞 fix(db):

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,5 @@ set -o errexit
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
 bundle install
-bundle exec rails db:prepare 
 bundle exec rake db:migrate --trace
 bundle exec rake db:seed
